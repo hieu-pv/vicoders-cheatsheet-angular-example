10104a0efa70888c52f6679dd5c33eaeb65381d2
hieupv <hieupv@codersvn.com>
2019-12-31 23:32:50 +0700

    save state to localStoreage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from './store/store.module';
+import initSubscriber from 'redux-subscriber';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  public title = 'app';
+  public store;
+  unsubscribe: any;
+  constructor(store: Store) {
+    this.store = store.getInstance();
+  }
+
+  ngOnInit(): void {
+    const saveState = state => {
+      try {
+        const serializedState = JSON.stringify(state);
+        localStorage.setItem('State', serializedState);
+      } catch {}
+    };
+    const subscribe = initSubscriber(this.store);
+
+    // Listen on change of state
+    const StateToListen = 'App';
+    this.unsubscribe = subscribe(StateToListen, state => {
+      saveState(state.App);
+    });
+  }
+
+  change() {
+    this.store.dispatch({ type: 'CHANGE', data: 'new value' });
+  }
+
+  reset() {
+    localStorage.removeItem('State');
+    console.log('localStorate is cleared');
+  }
 }
--- src/app/store/reducers.ts
+++ src/app/store/reducers.ts
@@ -1,8 +1,27 @@
 import * as _ from 'lodash';
 import { combineReducers } from 'redux';
 
-const App = (state, action) => {
+let defaultState = { initValue: 'NF' };
+
+if (!_.isNil(localStorage.getItem('State'))) {
+  const loadState = state => {
+    try {
+      const serializedState = localStorage.getItem(state);
+      if (serializedState === null) {
+        return undefined;
+      }
+      return JSON.parse(serializedState);
+    } catch (err) {
+      return undefined;
+    }
+  };
+  defaultState = _.assign(loadState('State'), { is_restored: true });
+}
+
+const App = (state = defaultState, action) => {
   switch (action.type) {
+    case 'CHANGE':
+      return _.assign({}, state, { initValue: action.data });
     default:
       return state;
   }
